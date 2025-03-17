f6b83706c591926fd4c10734d5b9dc4d8ac8d57f
Romith <romith.jinawardana@sysco.com>
2025-03-17 12:51:24 +0530

    Improved the list view to be expandable.

--- src/main/java/com/plugin/json2form/service/JsonConverter.java
+++ src/main/java/com/plugin/json2form/service/JsonConverter.java
@@ -17,22 +17,16 @@ public class JsonConverter {
         if (buildTopToBottom) {
             html.append(markupHeaderRow(jsonInput.keySet()));
             html.append("<tr>");
-            for (Object value : jsonInput.values()) {
-                if (value instanceof List) {
-                    html.append(maybeClub((List<?>) value));
-                } else {
-                    html.append(markupTableCell(value));
-                }
+            for (var entry : jsonInput.entrySet()) {
+                html.append(markup(entry.getValue(), entry.getKey()));
             }
             html.append("</tr>");
+            html.append("</table>");
+            return html.toString();
         } else {
             for (Map.Entry<String, Object> entry : jsonInput.entrySet()) {
-                html.append("<tr><th>").append(markup(entry.getKey())).append("</th>");
-                if (entry.getValue() instanceof List) {
-                    html.append(maybeClub((List<?>) entry.getValue()));
-                } else {
-                    html.append(markupTableCell(entry.getValue()));
-                }
+                html.append("<tr><th>").append(markup(entry.getKey(), "")).append("</th>");
+                markup(entry.getValue(), "");
                 html.append("</tr>");
             }
         }
@@ -41,29 +35,98 @@ public class JsonConverter {
         return html.toString();
     }
     
-    private String markupTableCell(Object value) {
-        boolean hasChildren = checkHasChildren(value);
-        String cellContent = markup(value);
-        String toggleContent = wrapWithToggle(cellContent, hasChildren);
-        String cellClass = hasChildren ? " class=\"has-children\"" : "";
-        return String.format("<td%s>%s</td>", cellClass, toggleContent);
+    private String markup(Object entry, String key) {
+        return switch (entry) {
+            case null -> markupCell("null", key);
+            case List list -> markup(list, key);
+            case Map map -> //noinspection unchecked
+                    markup((Map<String, Object>)map);
+            default -> markupCell(entry, key);
+        };
     }
-    
-    private boolean checkHasChildren(Object value) {
-        if (value instanceof Map) {
-            return !((Map<?, ?>) value).isEmpty();
-        }
-        if (value instanceof List) {
-            List<?> list = (List<?>) value;
-            return !list.isEmpty();
-        }
-        return false;
+
+    private String markupCell(Object value, String key) {
+        return String.format("<td mkey='%s'>%s</td>", key, value);
     }
-    
+
+    private String markup(Map<String, Object> map) {
+        StringBuilder html = new StringBuilder("<div class=\"list-wrapper\">");
+        html.append("<table>");
+        html.append(markupHeaderRow(map.keySet()));
+        html.append("<tr>");
+        for (String key : map.keySet()) {
+            html.append(markup(map.get(key), key));
+        }
+        html.append("</tr>");
+        html.append("</table>");
+        return wrapExpandable(html.toString());
+    }
+
+    private String markup(List<?> entry, String key) {
+        List<?> list = entry;
+        Set<String> commonHeaders = getCommonHeaders(entry);
+        if (commonHeaders != null) {
+            return clubListEntries(list, commonHeaders);
+        }
+        // list doesn't have common headers so just display the list
+        StringBuilder html = new StringBuilder(tableOpeningTag);
+        html.append("<tr>");
+        for (Object o : list) {
+            html.append(markup(o, key));
+        }
+        html.append("</tr>");
+        html.append("</table>");
+        return wrapExpandable(html.toString());
+    }
+
+    private String clubListEntries(List<?> listOfDicts, Set<String> columnHeaders) {
+        StringBuilder html = new StringBuilder(tableOpeningTag);
+        html.append(markupHeaderRow(columnHeaders));
+
+        for (Object item : listOfDicts) {
+            @SuppressWarnings("unchecked")
+            Map<String, Object> dict = (Map<String, Object>) item;
+            html.append("<tr>");
+            for (String columnHeader : columnHeaders) {
+                html.append(markup(dict.get(columnHeader), columnHeader));
+            }
+            html.append("</tr>");
+        }
+
+        html.append("</table>");
+        return wrapExpandable(html.toString());
+    }
+
+    private Set<String> getCommonHeaders(List<?> listOfDicts) {
+        Set<String> headers;
+        if (listOfDicts.isEmpty()) {
+            return null; // no common headers
+        }
+        if (listOfDicts.getFirst() instanceof Map map) {
+           headers = map.keySet();
+        } else {
+           return null; // cannot determine common headers
+        }
+
+        for (int i = 1; i < listOfDicts.size(); i++) {
+            Object item = listOfDicts.get(i);
+            if (!(item instanceof Map)) {
+                return null; // cannot determine common headers if entries are not dictionaries
+            }
+            @SuppressWarnings("unchecked")
+            Map<String, Object> dict = (Map<String, Object>) item;
+            if (!headers.equals(dict.keySet())) {
+                return null; // there are additional or missing headers
+            }
+        }
+
+        return headers;
+    }
+
     private String markupHeaderRow(Collection<String> headers) {
         return "<tr><th>" + String.join("</th><th>", headers) + "</th></tr>";
     }
-    
+
     private static String dictToHtmlAttributes(Map<String, String> attributes) {
         if (attributes == null || attributes.isEmpty()) {
             return "";
@@ -74,86 +137,20 @@ public class JsonConverter {
         }
         return result.toString();
     }
-    
-    private String markup(Object entry) {
-        if (entry == null) {
-            return "";
-        }
-        if (entry instanceof List) {
-            List<?> list = (List<?>) entry;
-            StringBuilder listMarkup = new StringBuilder("<div class=\"list-wrapper\">");
-            listMarkup.append("<ul>");
-            for (Object item : list) {
-                listMarkup.append("<li>").append(markup(item)).append("</li>");
-            }
-            listMarkup.append("</ul></div>");
-            return listMarkup.toString();
-        }
-        if (entry instanceof Map) {
-            @SuppressWarnings("unchecked")
-            Map<String, Object> map = (Map<String, Object>) entry;
-            return convert(map);
-        }
-        return String.valueOf(entry);
+
+    private String wrapExpandable(String cellContent) {
+        String toggleContent = wrapWithToggle(cellContent);
+        String cellClass = " class=\"has-children\"";
+        return String.format("<td%s>%s</td>", cellClass, toggleContent);
     }
-    
-    private String maybeClub(List<?> listOfDicts) {
-        Set<String> columnHeaders = getCommonHeaders(listOfDicts);
-        if (columnHeaders == null) {
-            return markupTableCell(markup(listOfDicts));
-        }
-        
-        StringBuilder html = new StringBuilder(tableOpeningTag);
-        html.append(markupHeaderRow(columnHeaders));
-        
-        for (Object item : listOfDicts) {
-            @SuppressWarnings("unchecked")
-            Map<String, Object> dict = (Map<String, Object>) item;
-            html.append("<tr><td>");
-            html.append(String.join("</td><td>", 
-                columnHeaders.stream()
-                    .map(header -> markup(dict.get(header)))
-                    .toArray(String[]::new)));
-            html.append("</td></tr>");
-        }
-        
-        html.append("</table>");
-        return markupTableCell(html.toString());
-    }
-    
-    private Set<String> getCommonHeaders(List<?> listOfDicts) {
-        if (listOfDicts.size() < 2 || !(listOfDicts.get(0) instanceof Map)) {
-            return null;
-        }
-        
-        @SuppressWarnings("unchecked")
-        Set<String> headers = new HashSet<>(((Map<String, Object>) listOfDicts.get(0)).keySet());
-        
-        for (int i = 1; i < listOfDicts.size(); i++) {
-            Object item = listOfDicts.get(i);
-            if (!(item instanceof Map)) {
-                return null;
-            }
-            @SuppressWarnings("unchecked")
-            Map<String, Object> dict = (Map<String, Object>) item;
-            if (!headers.equals(dict.keySet())) {
-                return null;
-            }
-        }
-        
-        return headers;
-    }
-    
-    private String wrapWithToggle(String content, boolean hasChildren) {
-        if (!hasChildren) {
-            return content;
-        }
+
+    private String wrapWithToggle(String content) {
         return String.format(
-            "<div class=\"cell-content\">" +
-            "<span class=\"toggle-btn expanded\" onclick=\"toggleNode(this)\"></span>" +
-            "<div class=\"content-wrapper\">%s</div>" +
-            "</div>", 
-            content
+                "<div class=\"cell-content\">" +
+                        "<span class=\"toggle-btn expanded\" onclick=\"toggleNode(this)\"></span>" +
+                        "<div class=\"content-wrapper\">%s</div>" +
+                        "</div>",
+                content
         );
     }
 } 
